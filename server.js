8b8f99c2904bb0d68b8f48e5f0371231fa8f0a27
Avinash Kumar <avinashk@vmokshagroup.com>
2016-12-19 15:23:21 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,4 @@
 //Initiallising Global variables and node modules
-var express = require("express");
-var mysql = require('mysql');
-var bodyParser = require("body-parser");
-//Getting Application Configuration
-//var appConfig = require('./config/appConfig.json');
-var app = express();
-//Getting utility functions
-//var utility = require('./common/utils')(appConfig);
-//To Store AuthToken
-//const _authToken = appConfig.authToken;
-
 var massive = require("massive");
 var connString = "postgres://lvrdoxuc:VwTN5BlZm6blo02-JtmnhI0YbQBC6TtO@elmer.db.elephantsql.com:5432/lvrdoxuc";
 
@@ -21,7 +10,7 @@ var connString = "postgres://lvrdoxuc:VwTN5BlZm6blo02-JtmnhI0YbQBC6TtO@elmer.db.
 //var db = app.get('db');
 //console.log(massiveInstance);
 
-var db = massive.connectSync({ connectionString: connString })
+var db = massive.connectSync({ connectionString: connString });
 
 var newUser = {
     email: "test@test.com",
@@ -45,9 +34,3 @@ db.users.find({ email: "test@test.com" }, function (err, res) {
     console.log("Search By Attribute");
     console.log(res);
 });
-
-//Setting up server
-var server = app.listen(process.env.PORT || 8000, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
- }); 
